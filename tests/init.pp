4777ec97cb1b88bbcca80b7fab75ae958968c209
Jeff McCune <jeff@puppetlabs.com>
2011-06-17 12:36:24 -0700

    Add java dependency to the test
    The ActiveMQ module needs java in place for the service to be brought
    online.  This change adds the java class declaration to the example test
    init.pp file.

--- tests/init.pp
+++ tests/init.pp
@@ -1,1 +1,9 @@
-include activemq
+node default {
+  notify { 'alpha': } ->
+  class  { 'java':
+    distribution => 'jdk',
+    version      => 'latest',
+  } ->
+  class  { 'activemq': } ->
+  notify { 'omega': }
+}
