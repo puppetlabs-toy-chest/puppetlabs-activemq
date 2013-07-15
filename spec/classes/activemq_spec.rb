7aa1627f54fe54802f95067d32b9c31123e50653
Richard Clamp <richardc@unixbeard.net>
2013-07-15 18:26:31 +0100

    test the webconsole parameter

--- spec/classes/activemq_spec.rb
+++ spec/classes/activemq_spec.rb
@@ -4,4 +4,23 @@ describe 'activemq' do
   it "should compile" do
     should contain_class('activemq')
   end
+
+  describe "#webconsole" do
+    context "with the default template" do
+      describe "true" do
+        let(:params) { { 'webconsole' => true } }
+        it { should contain_file('activemq.xml').with_content(/jetty.xml/) }
+        it {
+          pending "templates/default/jetty.xml is a tease - we don't actually push it out in any case"
+          should contain_file('jetty.xml')
+        }
+      end
+
+      describe "false" do
+        let(:params) { { 'webconsole' => false } }
+        it { should_not contain_file('activemq.xml').with_content(/jetty.xml/) }
+        it { should_not contain_file('jetty.xml') }
+      end
+    end
+  end
 end
