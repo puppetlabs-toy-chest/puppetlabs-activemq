abfe3f36d0183b90958a9479c35841186d4cf12f
Richard Clamp <richardc@unixbeard.net>
2013-07-16 11:20:25 +0100

    don't manage the activemq user
    Let the RPM/deb do it.

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -11,38 +11,12 @@
 # Sample Usage:
 #
 class activemq::packages (
-  $version,
-  $home = '/usr/share/activemq'
+  $version
 ) {
 
   validate_re($version, '^[._0-9a-zA-Z:-]+$')
-  validate_re($home, '^/')
 
   $version_real = $version
-  $home_real    = $home
-
-  # Manage the user and group in Puppet rather than RPM
-  group { 'activemq':
-    ensure => 'present',
-    gid    => '92',
-    before => User['activemq']
-  }
-  user { 'activemq':
-    ensure  => 'present',
-    comment => 'Apache Activemq',
-    gid     => '92',
-    home    => '/usr/share/activemq',
-    shell   => '/bin/bash',
-    uid     => '92',
-    before  => Package['activemq'],
-  }
-  file { $home_real:
-    ensure => directory,
-    owner  => '0',
-    group  => '0',
-    mode   => '0755',
-    before => Package['activemq'],
-  }
 
   package { 'activemq':
     ensure  => $version_real,
