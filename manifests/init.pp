43a7182f1aafb88793d9fb5da680babcfb26bb54
Bryan Ross <bryan@liquidstate.net>
2013-09-12 09:41:25 +0100

    added more configuration options to the template to set usernames, passwords and initial support to configure a network of brokers.

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,12 +25,17 @@
 # }
 #
 class activemq(
-  $version       = 'present',
-  $package       = 'activemq',
-  $ensure        = 'running',
-  $instance      = 'activemq',
-  $webconsole    = true,
-  $server_config = 'UNSET'
+  $version                 = 'present',
+  $package                 = 'activemq',
+  $ensure                  = 'running',
+  $instance                = 'activemq',
+  $webconsole              = true,
+  $server_config           = 'UNSET'
+  $mq_admin_username       = 'admin',
+  $mq_admin_password       = 'admin',
+  $mq_cluster_username     = 'amq',
+  $mq_cluster_password     = 'secret',
+  $mq_cluster_brokers      = [],
 ) {
 
   validate_re($ensure, '^running$|^stopped$')
@@ -41,6 +46,27 @@ class activemq(
   $version_real = $version
   $ensure_real  = $ensure
   $webconsole_real = $webconsole
+  $mq_admin_username_real       = $mq_admin_username
+  $mq_admin_password_real       = $mq_admin_password
+  $mq_cluster_username_real     = $mq_cluster_username
+  $mq_cluster_password_real     = $mq_cluster_password
+  $mq_cluster_brokers_real      = $mq_cluster_brokers
+
+  if $mq_admin_username_real == 'admin' {
+    warning '$mq_admin_username is set to the default value.  This should be changed.'
+  }
+
+  if $mq_admin_password_real == 'admin' {
+    warning '$mq_admin_password is set to the default value.  This should be changed.'
+  }
+
+  if size($mq_cluster_brokers_real) > 0 and $mq_cluster_username_real == 'amq' {
+    warning '$mq_cluster_username is set to the default value.  This should be changed.'
+  }
+
+  if size($mq_cluster_brokers_real) > 0 and $mq_cluster_password_real == 'secret' {
+    warning '$mq_cluster_password is set to the default value.  This should be changed.'
+  }
 
   # Since this is a template, it should come _after_ all variables are set for
   # this class.
