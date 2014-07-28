53717db9f642f2cd34e1ed42b51f61fd32014220
Alex Dreyer <alex@puppetlabs.com>
2014-07-28 16:02:33 -0700

    console does not work in debian

---
+++ manifests/params.pp
@@ -0,0 +1,24 @@
+# Private
+class activemq::params {
+  $version             = 'present'
+  $package             = 'activemq'
+  $ensure              = 'running'
+  $instance            = 'activemq'
+  $server_config       = 'UNSET'
+  $mq_broker_name      = $::fqdn
+  $mq_admin_username   = 'admin'
+  $mq_admin_password   = 'admin'
+  $mq_cluster_username = 'amq'
+  $mq_cluster_password = 'secret'
+  $mq_cluster_brokers  = []
+
+  # Debian does not include the webconsole
+  case $::osfamily {
+    'Debian': {
+      $webconsole = false
+    }
+    default: {
+      $webconsole = true
+    }
+  }
+}
